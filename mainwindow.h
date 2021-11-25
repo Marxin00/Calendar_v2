7b979310c7618d3b168026e8d4f44ba91b1b7d4a
Marcin <92454863+Marxin00@users.noreply.github.com>
2021-11-25 17:33:23 +0100

    V1.1

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QDate>
+#include <math.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +23,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QString plik="";
 };
 #endif // MAINWINDOW_H
